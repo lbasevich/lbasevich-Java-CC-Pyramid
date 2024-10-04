7b4c0872844abe738f4df0702cc7471acf6c7b31
Leo Basevich <lbasevich86@gmail.com>
2024-10-04 19:10:05 +0000

    Complete

--- src/main/java/Pyramid.java
+++ src/main/java/Pyramid.java
@@ -28,6 +28,15 @@ public class Pyramid {
      * @return a string representation of the pyramid.
      */
     public String returnPyramid(int n){
-        return "";
+        String result = new String();
+
+        for (int i = 1; i <= n; i++){
+            result+='\n';
+            for (int j = 1; j <= i; j++){
+                result+='*';
+            }
+        }
+
+        return result;
     }
 }
